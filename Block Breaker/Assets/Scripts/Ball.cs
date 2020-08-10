55383baa6f2b5ce04278de143c36f2b10f7c6082
Frederick Bouchard <42101543+Nerdshh@users.noreply.github.com>
2020-08-10 01:58:27 -0700

    added starting state and launch

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] Paddle paddleOne;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    // States
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddleOne.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }  
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddleOne.transform.position.x, paddleOne.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
